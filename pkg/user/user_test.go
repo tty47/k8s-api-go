488c23fc1ecce83c1f1fcfcd3782e0bb24d83695
Jose Ramon Mañes <jr.mb47@gmail.com>
2021-01-16 16:14:08 +0100

    add user struct tests

--- pkg/user/user_test.go
+++ pkg/user/user_test.go
@@ -1,1 +1,36 @@
 package user
+
+import "testing"
+
+func Test_User(t *testing.T) {
+	user := User{
+		ID:       0,
+		UserName: "jrmanes",
+		Role:     "devops",
+	}
+
+	t.Run("check user id", func(t *testing.T) {
+		got := user.ID
+		var want int32 = 0
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+	t.Run("validate username", func(t *testing.T) {
+		got := user.UserName
+		want := "jrmanes"
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+	t.Run("validate user role", func(t *testing.T) {
+		got := user.Role
+		want := "devops"
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+}
