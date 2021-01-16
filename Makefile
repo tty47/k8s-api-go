976c9eda3a281ef88d1a3048ff0987fae3b665f2
Jose Ramon Mañes <jr.mb47@gmail.com>
2021-01-16 17:36:50 +0100

    change uri to get them from env vars

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 PROJECT_NAME := k8s-api-go
 REGISTRY_NAME := jrmanes
 
+all: test build deploy
+
 build:
 	docker build . -f Dockerfile -t $(REGISTRY_NAME)/$(PROJECT_NAME):latest
 	docker push $(REGISTRY_NAME)/$(PROJECT_NAME):latest
@@ -10,3 +12,9 @@ test:
 
 deploy:
 	kubectl apply -f ./infrastructure/
+
+clean:
+	kubectl delete -f ./infrastructure/
+
+port_forward:
+	kubectl port-forward service/$(PROJECT_NAME)-svc 8080:8080 -n $(PROJECT_NAME)
--- internal/data/postgres.go
+++ internal/data/postgres.go
@@ -12,8 +12,11 @@ type PostgresDB struct {
 
 // getConnection open connection against PSQL database
 func getConnection() (*sql.DB, error) {
-	uri := os.Getenv("DATABASE_URI")
-	return sql.Open(os.Getenv("DB_DRIVER"), uri)
+	// generate the uri using env vars
+	uri := os.Getenv("DB_DRIVER") + "://" + os.Getenv("DB_USER") +":" +
+		os.Getenv("DB_PASSWORD") +"@" +os.Getenv("DB_HOST") +":"+ os.Getenv("DB_PORT") +
+		"/"+os.Getenv("DB_NAME")+"?&sslmode=disable"
+	return sql.Open(os.Getenv("DB_DRIVER") , uri)
 }
 
 // MakeMigration setup the database structure
